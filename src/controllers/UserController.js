2b60c1c3304a9392647a865d424a7918e820158e
Diego <diegoalencar.jacober@gmail.ocm>
2023-06-22 19:26:39 -0300

    Alterações de seguranças

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,8 +4,8 @@ class UserController {
   async store(req, res) {
     try {
       const user = await User.create(req.body);
-
-      return res.status(200).json(user);
+      const { id, email, password } = user;
+      return res.status(200).json({ id, email, password });
     } catch (error) {
       return res.status(400).json({
         errors: error.errors.map((e) => e.message),
@@ -15,7 +15,7 @@ class UserController {
 
   async index(req, res) {
     try {
-      const users = await User.findAll();
+      const users = await User.findAll({ attributes: ['id', 'nome', 'email'] });
       return res.status(200).json(users);
     } catch (error) {
       return res.json(null);
@@ -24,9 +24,9 @@ class UserController {
 
   async show(req, res) {
     try {
-      const { id } = req.params;
-      const user = await User.findByPk(id);
-      return res.status(200).json(user);
+      const user = await User.findByPk(req.params.id);
+      const { id, nome, email } = user;
+      return res.status(200).json({ id, nome, email });
     } catch (error) {
       return res.status(404).json(null);
     }
@@ -34,15 +34,13 @@ class UserController {
 
   async update(req, res) {
     try {
-      const { id } = req.params;
-
-      if (!id) {
+      if (!req.params.id) {
         return res.status(400).json({
           error: ['Informe o id de qual usuário deseja atualizar'],
         });
       }
 
-      const user = await User.findByPk(id);
+      const user = await User.findByPk(req.params.id);
 
       if (!user) {
         return res.status(404).json({
@@ -51,9 +49,9 @@ class UserController {
       }
 
       const novosDados = await user.update(req.body);
-      return res.status(200).json(novosDados);
+      const { id, email, password } = novosDados;
+      return res.status(200).json({ id, email, password });
     } catch (error) {
-      console.log(error);
       return res.status(500).json(null);
     }
   }
--- src/routes/user.js
+++ src/routes/user.js
@@ -4,11 +4,13 @@ import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
+// Não deveriam existir
 router.post('/', userController.store);
 router.get('/', loginRequired, userController.index);
+
 router.get('/:id', userController.show);
-router.put('/:id', userController.update);
-router.delete('/:id', userController.delete);
+router.put('/:id', loginRequired, userController.update);
+router.delete('/:id', loginRequired, userController.delete);
 
 export default router;
 
