235c0fcb14c60c26972999cdf7cabd59e7ec76cc
ETS-EngineeringTechnicalSchool BOT-ResearchDevelopment (CaP/ETS) <>
2023-06-27 13:51:51 -0300

    ...

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 module.exports = {
   dialect: 'mysql',
-  host: 'database',
-  port: 3306,
-  username: 'diego',
-  password: '123456',
-  database: 'escola',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
